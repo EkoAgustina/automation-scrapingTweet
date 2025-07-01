84b63d0357d30b9716c549c25589266d0dfc3804
EkoAgustina <eko.agustina86@gmail.com>
2025-07-01 11:35:57 +0700

    First commit

--- cucumber/step-definitions/steps.ts
+++ cucumber/step-definitions/steps.ts
@@ -41,7 +41,7 @@ When(/^User click "(.*)"$/, async (locator) => {
 
 When(/^Users do scraping twitter data$/,async () => {
     try {
-        await runTweetScrapingLoops(100)
+        await runTweetScrapingLoops(50)
     } catch (err: any) {
         log("ERROR", err.message)
         throw err
