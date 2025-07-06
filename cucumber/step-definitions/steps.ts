c7c3f367c5e30661981c9039177dc882258a07d6
EkoAgustina <eko.agustina86@gmail.com>
2025-07-06 08:42:20 +0700

    Update handling for similar tweets

--- cucumber/step-definitions/steps.ts
+++ cucumber/step-definitions/steps.ts
@@ -41,7 +41,7 @@ When(/^User click "(.*)"$/, async (locator) => {
 
 When(/^Users do scraping twitter data$/,async () => {
     try {
-        await runTweetScrapingLoops(2)
+        await runTweetScrapingLoops(260)
     } catch (err: any) {
         log("ERROR", err.message)
         throw err
