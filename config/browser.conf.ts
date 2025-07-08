96293b179b3e85e695c82bdf6083ff17257ab626
EkoAgustina <eko.agustina86@gmail.com>
2025-07-08 11:45:50 +0700

    clean up code structure and improve variable naming for better readability

--- config/browser.conf.ts
+++ config/browser.conf.ts
@@ -20,7 +20,7 @@ if (globalVariables.os === 'linux') {
                             '--disable-dev-shm-usage',
                             '--disable-extensions',
                             '--disable-cache', 
-                            '--remote-debugging-pipe',
+                            // '--remote-debugging-pipe',
                             `--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36`]
             },
             acceptInsecureCerts: true,
@@ -85,7 +85,7 @@ if (globalVariables.os === 'linux') {
                             '--disable-dev-shm-usage',
                             '--disable-extensions',
                             '--disable-cache', 
-                            '--remote-debugging-pipe',
+                            // '--remote-debugging-pipe',
                             `--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36`]
                     },
                     acceptInsecureCerts: true,
